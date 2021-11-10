676208b6d5bf863db1b60e5559c074ed039d30bc
matbravc <matjaz.bravc@endava.com>
2021-11-10 19:30:46 +0100

    Minor refactoring

--- src/Services.Gateway/Program.cs
+++ src/Services.Gateway/Program.cs
@@ -1,7 +1,8 @@
-using System.IO;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace Endava.Services.Gateway
 {
@@ -9,23 +10,31 @@ namespace Endava.Services.Gateway
 	{
 		public static void Main(string[] args)
 		{
-			BuildWebHost(args).Run();
+			CreateHostBuilder(args).Build().Run();
 		}
 
-		public static IWebHost BuildWebHost(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
-				.UseKestrel()
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
 				.UseContentRoot(Directory.GetCurrentDirectory())
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				})
 				.ConfigureAppConfiguration((hostingContext, config) =>
 				{
 					config
 						.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-						.AddJsonFile("appsettings.json", true, true)
+						.AddJsonFile("appsettings.json", false, true)
 						.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
 						.AddJsonFile("ocelot.json", false, false)
 						.AddEnvironmentVariables();
 				})
-				.UseStartup<Startup>()
-				.Build();
+				.ConfigureLogging((builderContext, logging) =>
+				{
+					logging.AddConfiguration(builderContext.Configuration.GetSection("Logging"));
+					logging.AddConsole();
+					logging.AddDebug();
+					logging.AddEventSourceLogger();
+				});
 	}
 }
--- src/Services.Gateway/Startup.cs
+++ src/Services.Gateway/Startup.cs
@@ -22,7 +22,6 @@ namespace Endava.Services.Gateway
 				.AddPolly();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			app.UseOcelot().Wait();
